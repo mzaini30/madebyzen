c5f0c9a74bde8382f4c16371c80b81d4a7f179b6
mzaini30 <muhzaini30@gmail.com>
2022-12-10 01:25:37 +0800

    Termux

--- iles.config.ts
+++ iles.config.ts
@@ -4,6 +4,7 @@ import transformerDirectives from "@unocss/transformer-directives";
 
 export default defineConfig({
   svelte: true,
+  siteUrl: "https://madebyzen.js.org",
   vite: {
     optimizeDeps: {
       include: ["svelte"],

f49c2ea376cf8ec9f3e41fd60398401f56838142
mzaini30 <muhzaini30@gmail.com>
2022-12-10 08:02:25 +0800

    Termux

--- src/pages/tag-youtube.vue
+++ src/pages/tag-youtube.vue
@@ -4,4 +4,5 @@
 
 <template>
   <TagYoutube client:only></TagYoutube>
+  <div class="!hidden mb-2 block w-full block border border-violet-800 p-2 inline-block mt-3 bg-red-500 text-white p-2 text-red-500 "></div>
 </template>
